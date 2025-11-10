3cda65e2e88f3e1f60e32e083ea9ea0be96f8b4c
unknown <your@email.example>
2025-11-10 16:16:00 +0200

    added file coffee-order.js

---
+++ coffee-order.js
@@ -0,0 +1,10 @@
+let coffeSize = "Large"
+let addSugar = true;
+
+    if(coffeSize === "Small"){
+        console.log("kafija maksā 1 eiro un 20 centi")
+    }   else if (coffeSize === "Medium") {
+        console.log("kafija maksā 1 eiro un 70 centi")
+    } else {
+        console.log("kafija maksā 2 eiro un 20 centi")
+    }
