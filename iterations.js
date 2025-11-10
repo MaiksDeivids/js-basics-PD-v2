6b0b214a6d13076622ed61df996a9c388ed8a1e3
unknown <your@email.example>
2025-11-10 16:10:04 +0200

    first commit

---
+++ iterations.js
@@ -0,0 +1,11 @@
+for(i = 17; i <= 21; i++){
+    console.log("skaitlis:" + i)
+}
+console.log("========")
+
+    let j = 17
+        while(j > 11){
+            j --;
+            console.log(j + "reizes")
+        }
+
